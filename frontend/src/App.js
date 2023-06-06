a30eefd705c8b9c15821dcf3b110cc2704dc5a40
dflorr00 <dflorr00@estudiantes.unileon.es>
2023-06-06 18:40:34 +0200

    Resuelto problema de rutas en app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,7 @@ import Inicio from './pages/publico/inicioSesion';
 import Registro from './pages/publico/registro';
 import Home from './pages/privado/home';
 import Pago from './pages/privado/pago';
-import Ordenadores from './pages/privado/ordenadores';
-import Perifericos from './pages/privado/perifericos';
-import Smartphones from './pages/privado/smartphones';
-import Televisores from './pages/privado/productos';
+import Productos from './pages/privado/productos';
 import PagoRealizado from './pages/privado/pagoRealizado';
 import LoginChecker from './servidor/loginChecker';
 
@@ -23,10 +20,7 @@ function App() {
         <Route path='/registro' element={<Registro />}></Route>
         <Route path='/home' element={<Home />}></Route>
         <Route path='/pago' element={<Pago />}></Route>
-        <Route path='/ordenadores' element={<Ordenadores />}></Route>
-        <Route path='/perifericos' element={<Perifericos />}></Route>
-        <Route path='/televisores' element={<Televisores />}></Route>
-        <Route path='/smartphones' element={<Smartphones />}></Route>
+        <Route path='/productos' element={<Productos />}></Route>
         <Route path='/pagoRealizado' element={<PagoRealizado />}></Route>
       </Routes>
     </Router>
